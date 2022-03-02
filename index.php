67da1430a9fb9e14e7e94baa919cb662eb36712b
DEV-Blocki2006 <85703281+DEV-Blocki2006@users.noreply.github.com>
2022-03-02 11:44:49 +0100

    da

--- index.php
+++ index.php
@@ -2,10 +2,22 @@
 <html lang="en" dir="ltr">
   <head>
     <?php // IDEA: css ?>
+    <link rel="stylesheet" href="assets/css/startpage.css">
+    <link rel="stylesheet" href="assets/css/news.css">
 
 
     <?php // IDEA: fonts ?>
-    <link rel="stylesheet" href="assets/css/startpage.css">
+      <?php // IDEA: font-family: 'Bebas Neue', cursive; ?>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+      <?php // IDEA: font-family: 'Kanit', sans-serif; ?>
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@500&display=swap" rel="stylesheet">
+
+
+
 
 
     <?php // IDEA: responable ?>
@@ -17,6 +29,64 @@
     <title>ToShortDE - home</title>
   </head>
   <body>
+
+    <div class="HEADER">
+
+    </div>
+
+    <?php // IDEA: Startpage ?>
+      <div class="Startpage">
+        <div class="Simg">
+          hallo
+        </div>
+      </div>
+
+      <div class="partner">
+        <div class="partner-img">
+          <a href="https://www.fastmc.net/"><img src="https://www.fastmc.net/assets/img/logo.png" alt=""></a>
+        </div>
+        <div class="partner-img">
+          <a href="https://www.fastmc.net/"><img src="https://www.fastmc.net/assets/img/logo.png" alt=""></a>
+        </div>
+        <div class="partner-img">
+          <a href="https://www.fastmc.net/"><img src="https://www.fastmc.net/assets/img/logo.png" alt=""></a>
+        </div>
+        <div class="partner-img">
+          <a href="https://www.fastmc.net/"><img src="https://www.fastmc.net/assets/img/logo.png" alt=""></a>
+        </div>
+        <div class="partner-img">
+          <a href="https://www.fastmc.net/"><img src="https://www.fastmc.net/assets/img/logo.png" alt=""></a>
+        </div>
+        <div class="partner-img">
+          <a href="https://www.fastmc.net/"><img src="https://www.fastmc.net/assets/img/logo.png" alt=""></a>
+        </div>
+        <div class="partner-img">
+          <a href="https://www.fastmc.net/"><img src="https://www.fastmc.net/assets/img/logo.png" alt=""></a>
+        </div>
+        <div class="partner-img">
+          <a href="https://www.fastmc.net/"><img src="https://www.fastmc.net/assets/img/logo.png" alt=""></a>
+        </div>
+        <div class="partner-img">
+          <a href="https://www.fastmc.net/"><img src="https://www.fastmc.net/assets/img/logo.png" alt=""></a>
+        </div>
+      </div>
+
+      <div class="news">
+        <h1>The newest news</h1>
+        <div class="news-div">
+          <div class="news-in">
+
+          </div>
+          <div class="news-in">
+            <div class="news-img">
+              <img src="https://forum.toshort.de/images/bg.png" alt="">
+            </div>
+          </div>
+          <div class="news-in">
+
+          </div>
+        </div>
+      </div>
 
   </body>
 </html>
