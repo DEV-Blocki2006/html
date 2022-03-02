290881b067b1e391e6c1e05b371b14d82a80af47
DEV-Blocki2006 <85703281+DEV-Blocki2006@users.noreply.github.com>
2022-03-02 09:32:37 +0100

    s

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <?php // IDEA: css ?>
+
+
+    <?php // IDEA: fonts ?>
+    <link rel="stylesheet" href="assets/css/startpage.css">
+
+
+    <?php // IDEA: responable ?>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+
+
+    <meta charset="utf-8">
+    <title>ToShortDE - home</title>
+  </head>
+  <body>
+
+  </body>
+</html>
